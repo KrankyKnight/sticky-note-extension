7fd4bd4eac65cd7e3d9afa8a85f4a1d24c96940f
Donald Twiford <donaldptwiford@gmail.com>
2023-05-17 20:14:41 -0700

    initialization of sticky note extension

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// create a function to create a sticky note
+// creae a eventlistner "click" to create a stick note
+// another eventlistner  
+
+//DOM listener
+document.addEventListener('DOMContentLoaded', () => {
+    //create a new html element button in the existing body
+    const body = document.querySelector('body');
+    body.setAttribute('class', 'whoKnows');
+    // const button = body.appendChild(document.createElement('button'));
+    // button.setAttribute('id', 'stickyNoteButton'); //giving button an ID of stickyNoteButton
+    // body.addEventListener('click', function () {
+    //     // document.getElementById('id')
+    //     alert('test');
+    // })
+    
+    
+    //when clicked 
+        //create sticky note element
+});
